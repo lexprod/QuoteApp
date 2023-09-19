ce15ea97dea735d81d7bdf776cf3b686e0227543
Alex Colby <lexprod@gmail.com>
2023-09-19 13:40:59 -0700

    styling

--- App.js
+++ App.js
@@ -18,18 +18,45 @@ import axios from 'axios';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up QUOTES!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.supercontainer}>
+      <View style={styles.container}>
+        <Text style={styles.quotetext}>Quote!</Text>
+        <Text style={styles.credittext}>--author!</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>Open up QUOTES!</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>Open up QUOTES!</Text>
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    borderColor: '#444',
+    borderWidth: 5,
+    flexDirection: 'column',
+    margin: 10,
+    padding: 10,
+    width: '80%',
+    backgroundColor: '#ddd',
+  },
+  supercontainer: {
+    margin: 40,
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+    alignContent: 'space-around'
   },
+  quotetext: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  credittext: {
+    fontStyle: 'italic',
+    fontSize: 14
+  }
 });
