0c324c1d330d7dccbf6a8087d28500071b1dd86a
Alex Colby <lexprod@gmail.com>
2023-09-19 12:48:49 -0700

    axios adding

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+// async function getMoviesFromApi() {
+//   try {
+//     let response = await fetch(
+//       'Your URL to fetch data from',
+//     );
+//     let responseJson = await response.json();
+//     console.log(responseJSon);
+//     return responseJson;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
 
 export default function App() {
   return (
